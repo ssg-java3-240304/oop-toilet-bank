18ae5b0d1b6f18f086f5378ab398d9b325897268
jectgenius <jkl9087@naver.com>
2024-04-04 01:01:32 +0900

    Feat: 은행 업무 입력 메소드 추가 #38

--- src/main/java/toiletbank/domain/ToiletBank.java
+++ src/main/java/toiletbank/domain/ToiletBank.java
@@ -1,4 +1,44 @@
 package toiletbank.domain;
 
-public class ToiletBank implements Bank{
+import toiletbank.domain.account.SavingsAccount;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class ToiletBank implements Bank {
+    private Scanner sc = new Scanner(System.in);
+
+    public void menu() {    // 은행 업무 선택 메소드
+
+        // String block문법 (여러줄, 들여쓰기 표현가능)
+        String menu = """
+                =========================
+                   은행 업무를 선택해주세요.
+                =========================
+                1. 계좌 잔액 조회
+                2. 계좌 이체
+                3. Toilet Bank 계좌 생성
+                4. Toilet Bank 계좌 삭제
+                5. 종료
+                =========================
+                입력 : """;
+
+        while (true) {
+            System.out.print(menu);
+            String choice = sc.next();
+            switch (choice) {
+                case "1":
+                    // createAccount();
+                    break;
+                case "2": // driver.accelerate(); break;
+                case "3": // driver.brake(); break;
+                case "4": // driver.stopEngine(); break;
+                case "5":
+                    return; // 현재메소드를 호출한 곳을 리턴
+                default:
+                    System.out.println("> 잘못 입력하셨습니다.");
+            }
+        }
+    }
+
 }
