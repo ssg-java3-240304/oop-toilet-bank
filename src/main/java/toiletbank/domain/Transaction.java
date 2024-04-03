8af0be0f8a67479bfa86b978bbabd44fa5398878
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-04 02:24:57 +0900

    feat(Transaction): override toString() #9

--- src/main/java/toiletbank/domain/Transaction.java
+++ src/main/java/toiletbank/domain/Transaction.java
@@ -1,11 +1,40 @@
 package toiletbank.domain;
 
 import java.math.BigInteger;
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Transaction {
     private LocalDateTime date;
     private BigInteger amount;
     private BigInteger balance;
 
+    public Transaction(LocalDateTime date, BigInteger amount, BigInteger balance) {
+        this.date = date;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public BigInteger getAmount() {
+        return amount;
+    }
+
+    public BigInteger getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("#,###");
+
+        return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm"))
+                + " ("+ df.format(amount)+ "원) "
+                + (amount.intValue()>0? "입금":"송금") + " /  "
+                + "잔액 "+df.format(balance)+"원";
+    }
 }
