155f938fb66c08e920ba7ca00d7285e372dd6eb1
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-04 16:57:41 +0900

    feat(toiletbank): create Customers class

---
+++ src/main/java/toiletbank/domain/Customers.java
@@ -0,0 +1,20 @@
+package toiletbank.domain;
+
+import toiletbank.domain.account.Account;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Customers implements Serializable {
+    private final Map<Customer, List<Account>> customers;
+
+    public Customers() {
+        this.customers = new HashMap<>();
+    }
+
+    public Map<Customer, List<Account>> getCustomers() {
+        return customers;
+    }
+}
