c8992829ba643ff4a6323bb31270c903af896bfe
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-04 23:58:42 +0900

    feat(Application): modify main method

--- src/main/java/toiletbank/Application.java
+++ src/main/java/toiletbank/Application.java
@@ -1,52 +1,13 @@
 package toiletbank;
 
-import toiletbank.domain.Customer;
-import toiletbank.domain.Transaction;
-import toiletbank.domain.Transactions;
-import toiletbank.domain.account.Account;
-import toiletbank.domain.account.SavingsAccount;
-import toiletbank.domain.bank.ToiletBank;
-import toiletbank.repository.Repository;
-import toiletbank.view.OutputView;
-
-import java.math.BigInteger;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import toiletbank.controller.Controller;
 
 public class Application {
     public static void main(String[] args) {
         // ❌❌❌❌❌❌❌❌❌❌❌❌❌ 주석 절대로 해제하지마세요. ❌❌❌❌❌❌❌❌❌❌❌❌❌
-//        Repository repository = new Repository();
+        //Repository repository = new Repository();
 
-        // test code 입니다. 삭제될 예정
-        ToiletBank toiletBank = new ToiletBank();
-        for (Map.Entry<Customer, List<Account>> customerListEntry : toiletBank.getCustomers().getCustomers().entrySet()) {
-            System.out.print(customerListEntry.getKey().getName());
-            for (Account account : customerListEntry.getValue()) {
-                System.out.println(account.getBalance());
-                System.out.println(account.getBank());
-                System.out.println(account.getType());
-            }
-        }
-
-        // transaction test code
-        Transactions transactions = new Transactions();
-        transactions.add(new Transaction(LocalDateTime.of(2024, 4,1,1,1), new BigInteger("-1000"), new BigInteger("10000")));
-        transactions.add(new Transaction(LocalDateTime.now(), new BigInteger("1000"), new BigInteger("10000")));
-        transactions.add(new Transaction(LocalDateTime.of(2024, 4,3,1,1), new BigInteger("-1000"), new BigInteger("10000")));
-
-//        System.out.println(transactions);
-
-
-        // printAccountDetails test code
-        OutputView outputView = new OutputView();
-
-        outputView.printAccountDetails(toiletBank.getAccountDetails(new Customer("변성일", "980101-2133336"),
-                toiletBank.getCustomers().getCustomers().get(new Customer("변성일", "980101-2133336")).get(0)),
-                transactions);
-
-        // printAccounts test code
-        outputView.printAccounts(toiletBank.getAccounts(new Customer("변성일", "980101-2133336")));
+        Controller controller = new Controller();
+        controller.run();
     }
 }
