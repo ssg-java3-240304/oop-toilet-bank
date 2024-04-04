8d252d1eb0bf638e151ade3d5ada9c34ae88364e
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-04-04 15:19:17 +0900

    feat(OutputView): create 계좌 상세히 조회 method #17

--- src/main/java/toiletbank/Application.java
+++ src/main/java/toiletbank/Application.java
@@ -4,8 +4,10 @@ import toiletbank.domain.Customer;
 import toiletbank.domain.Transaction;
 import toiletbank.domain.Transactions;
 import toiletbank.domain.account.Account;
+import toiletbank.domain.account.SavingsAccount;
 import toiletbank.domain.bank.ToiletBank;
 import toiletbank.repository.Repository;
+import toiletbank.view.OutputView;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
@@ -28,11 +30,23 @@ public class Application {
             }
         }
 
+        // transaction test code
         Transactions transactions = new Transactions();
-        transactions.add(new Transaction(LocalDateTime.now(),new BigInteger("1000"), new BigInteger("10000")));
-        transactions.add(new Transaction(LocalDateTime.now(),new BigInteger("-1000"), new BigInteger("10000")));
-        transactions.add(new Transaction(LocalDateTime.now(),new BigInteger("1000"), new BigInteger("10000")));
+        transactions.add(new Transaction(LocalDateTime.of(2024, 4,1,1,1), new BigInteger("-1000"), new BigInteger("10000")));
+        transactions.add(new Transaction(LocalDateTime.now(), new BigInteger("1000"), new BigInteger("10000")));
+        transactions.add(new Transaction(LocalDateTime.of(2024, 4,3,1,1), new BigInteger("-1000"), new BigInteger("10000")));
 
-        System.out.println(transactions);
+//        System.out.println(transactions);
+
+
+        // printAccountDetails test code
+        OutputView outputView = new OutputView();
+
+
+
+        System.out.println(toiletBank.getCustomers().get(new Customer("변성일", "980101-2133336")));
+        outputView.printAccountDetails(toiletBank.getAccountDetails(new Customer("변성일", "980101-2133336"),
+                toiletBank.getCustomers().get(new Customer("변성일", "980101-2133336")).get(0)),
+                transactions);
     }
 }
--- src/main/java/toiletbank/view/OutputView.java
+++ src/main/java/toiletbank/view/OutputView.java
@@ -1,5 +1,9 @@
 package toiletbank.view;
 
+import toiletbank.domain.Transactions;
+
+import java.util.List;
+
 public class OutputView {
 
     // 이동될 것 사용금지
@@ -9,5 +13,16 @@ public class OutputView {
                 보유하고 계신 입출금 통장이 없어, 정기 예금 또는 정기 적금 계좌 생성이 불가합니다.
                 먼저, 입출금 통장을 개설해주세요.""";
         System.out.println(str);
+    }
+
+    public void printAccountDetails(List<String> accountInfo, Transactions transactions) {
+        System.out.println("계좌 은행 : "+accountInfo.get(0));
+        System.out.println("계좌 타입 : "+accountInfo.get(1));
+        System.out.println("계좌 번호 : "+accountInfo.get(2));
+        System.out.println("이자율 : "+accountInfo.get(3));
+        System.out.println("잔액 : "+accountInfo.get(4));
+        System.out.println();
+        System.out.println("거래기록");
+        System.out.println(transactions);
     }
 }
